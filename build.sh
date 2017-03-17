a6de4294aabac708bfb003b6817003220e3c827f
Chris <christof@weinreich-edv.de>
2017-03-17 13:16:23 +0100

    bash script

--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-echo Step 1  - Build Bison parser
-bison -d compiler.y -b generated\compiler || exit 1;
+echo "Step 1 Bison Parser"
+bison -d compiler.y -b generated/compiler || { exit 1; }
 
-echo Step 2  - Build flex scanner
-flex -ogenerated\lex.yy.c compiler.l || exit 1;
+echo "Step 2 Flex Scanner"
+flex -o generated/lex.yy.c compiler.l || { exit 1; }
 
-echo Step 3  - Compile parser
-gcc -g -c generated\compiler.tab.c -o bin\compiler_y.o || exit 1;
+echo "Step 3 Compile Parser"
+gcc -g -c generated/compiler.tab.c -o bin/compiler_y.o || { exit 1; }
 
-echo Step 4  - Compile scanner
-gcc -g -c generated\lex.yy.c -o bin\lex.yy.o || exit 1;
+echo "Step 4 Compile Scanner"
+gcc -g -c generated/lex.yy.c -o bin/lex.yy.o || { exit 1; }
 
-echo Step 5a - Compile compiler.c
-gcc -g -c compiler.c -o bin\compiler.o || exit 1;
+echo "Step 5a Compiler.o"
+gcc -g -c compiler.c -o bin/compiler.o || { exit 1; }
 
-echo Step 5b - Compile symboltable.c
-gcc -g -c symboltable.c -o bin\symboltable.o || exit 1;
+echo "Step 5b Symboltabelle.o"
+gcc -g -c symboltable.c -o bin/symboltable.o || { exit 1; }
 
-echo Step 5c - Compile generator.c
-gcc -g -c generator.c -o bin\generator.o || exit 1;
+echo "Step 5c Generator.o"
+gcc -g -c generator.c -o bin/generator.o || { exit 1; }
 
-echo Step 5d - Compile optimizer.c
-gcc -g -c optimizer.c -o bin\optimizer.o || exit 1;
+echo "Step 5d Optimizer.o"
+gcc -g -c optimizer.c -o bin/optimizer.o || { exit 1; }
 
-echo Step 5e - Compile interpreter.c
-gcc -g -c interpreter.c -o bin\interpreter.o || exit 1;
+echo "Step 5e Interpeter.o"
+gcc -g -c interpreter.c -o bin/interpreter.o || { exit 1; }
 
-echo Step 6  - Link compile result
-gcc -g -o bin\compiler.exe bin\compiler.o bin\symboltable.o bin\generator.o bin\optimizer.o bin\interpreter.o bin\compiler_y.o bin\lex.yy.o -lm -lfl || exit 1;
+echo "Step 6 Link result"
+gcc -g -o bin/compiler bin/compiler.o bin/symboltable.o bin/generator.o bin/optimizer.o bin/interpreter.o bin/compiler_y.o bin/lex.yy.o -lm -ll || { exit 1; }
